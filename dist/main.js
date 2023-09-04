ca7d2d2c5436a5e644100a40d191cc70293ece6a
Maria Vanyushkina <m199178@mail.ru>
2023-09-04 13:30:25 +0300

    modified app, README

--- dist/main.js
+++ dist/main.js
@@ -364,36 +364,6 @@ class WidgetChat {
 const messagesBox = document.querySelector(".messages-box");
 const messageInput = document.querySelector(".message-input");
 const widgetChat = new WidgetChat(messagesBox, messageInput);
-window.addEventListener("beforeunload", () => {
-  widgetChat.messages.forEach(message => {
-    if (message.type === "audio" || message.type === "video") {
-      const reader = new FileReader();
-      reader.readAsArrayBuffer(message.value);
-      reader.onload = () => {
-        message.value = reader.result;
-        console.log(message.value);
-        localStorage.setItem("savedMessages", JSON.stringify(widgetChat.messages));
-      };
-    } else {
-      localStorage.setItem("savedMessages", JSON.stringify(widgetChat.messages));
-    }
-  });
-});
-document.addEventListener("DOMContentLoaded", () => {
-  const savedMessages = JSON.parse(localStorage.getItem("savedMessages"));
-  console.log(savedMessages);
-  if (savedMessages) {
-    savedMessages.forEach(message => {
-      if (message.type === "audio" || message.type === "video") {
-        console.log(message.value);
-        const blob = new Blob([message.value]);
-        message.value = URL.createObjectURL(blob);
-      }
-      widgetChat.messages.push(message);
-      widgetChat.messagesBox.prepend(widgetChat.renderMessage(message));
-    });
-  }
-});
 ;// CONCATENATED MODULE: ./src/index.js
 
 
